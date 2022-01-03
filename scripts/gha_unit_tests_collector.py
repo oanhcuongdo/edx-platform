d6fe6377920a5744b3e0617a64574b8126f0926b
mraarif <mraarif@outlook.com>
2022-01-03 17:16:24 +0500

    fix: updated the logic for unit test shards and test collection

--- scripts/gha_unit_tests_collector.py
+++ scripts/gha_unit_tests_collector.py
@@ -13,33 +13,27 @@ def get_all_unit_test_shards():
     return unit_test_workflow_shards
 
 
-def get_modules_except_cms():
+def get_unit_test_modules(module_name="lms"):
+    unit_test_modules = set()
     all_unit_test_shards = get_all_unit_test_shards()
-    return [shard_conf['path'] for shard_name, shard_conf in all_unit_test_shards.items() if not shard_conf['path'].startswith('cms')]
-
-
-def get_cms_modules():
-    all_unit_test_shards = get_all_unit_test_shards()
-    return [shard_conf['path'] for shard_name, shard_conf in all_unit_test_shards.items() if shard_conf['path'].startswith('cms')]
+    for shard_name, shard_confing in all_unit_test_shards.items():
+        if shard_confing.get('path')[0].startswith('cms') and module_name == "cms":
+            unit_test_modules.update(shard_confing.get('path'))
+        elif not shard_confing.get('path')[0].startswith("cms") and module_name != "cms":
+            unit_test_modules.update(shard_confing.get('path'))
+    return unit_test_modules
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cms-only", action="store_true", default="")
     parser.add_argument("--lms-only", action="store_true", default="")
-
     argument = parser.parse_args()
 
-    if argument.lms_only:
-        modules = get_modules_except_cms()
-    elif argument.cms_only:
-        modules = get_cms_modules()
-    else:
-        modules = []
+    if not argument.cms_only and not argument.lms_only:
+        print("Please specify --cms-only or --lms-only")
+        sys.exit(1)
 
-    unique_modules = set()
-    for module in modules:
-        unique_modules.update(set(module.split(" ")))
-
-    unit_test_paths = ' '.join(unique_modules)
-    sys.stdout.write(unit_test_paths)
+    modules = get_unit_test_modules("cms") if argument.cms_only else get_unit_test_modules("lms")
+    paths_output = ' '.join(modules)
+    sys.stdout.write(paths_output)
